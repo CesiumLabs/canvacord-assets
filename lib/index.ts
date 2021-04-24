c0fe87f664f40659293fdb806fdffe6cb3864ab0
ZYROUGE <zywouge@gmail.com>
2021-04-24 20:26:55 +0530

    Assets of Canvacord v6

---
+++ lib/index.ts
@@ -0,0 +1,35 @@
+import { promises as fs } from "fs";
+import path from "path";
+
+class AssetManager {
+    dir: string;
+    loaded: boolean;
+    data: Record<string, string>;
+
+    constructor(dir: string) {
+        this.dir = dir;
+        this.loaded = false;
+        this.data = {};
+    }
+
+    async load() {
+        const files = await fs.readdir(this.dir);
+
+        for (const file of files) {
+            const name = file.split(".")[0];
+            this.data[name] = `${this.dir}/${file}`;
+        }
+    }
+
+    get(name: string) {
+        return this.data[name];
+    }
+}
+
+export = {
+    font: new AssetManager(path.join(__dirname, "data", "fonts")),
+    image: new AssetManager(path.join(__dirname, "data", "images")),
+    utils: {
+        AssetManager
+    }
+};
